ce67fd344c8ef04155fda91f1ffc27e32a285181
tomaszskrok1 <126695915+tomaszskrok@users.noreply.github.com>
2023-03-29 15:11:00 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args)
+  {
+
+    for(int i = 0; i < 5; i++)
+      {
+        for(int j = 0; j < 5-i; j++)
+          {
+            System.out.print("");
+          }
+        for(int j = 0; j < i + 1; j++)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+      }  
+    
+    for(int i = 0; i < 5; i++)
+      {
+        for(int j = 0; j < 5-i-1; j++)
+          {
+            System.out.print(" ");
+          }
+        for(int j = 0; j < i+1; j++)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+      }  
+  }
+}
+
+
+  
